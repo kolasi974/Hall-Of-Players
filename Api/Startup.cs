8c56d4ea663bbc0226ae842d6e5ead142538adc2
Jérémy <jeremy-lebon974@outlook.fr>
2023-07-16 15:36:23 +0400

    Startup.cs

---
+++ Api/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Api;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        // Add Swagger/OpenAPI services
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
+
